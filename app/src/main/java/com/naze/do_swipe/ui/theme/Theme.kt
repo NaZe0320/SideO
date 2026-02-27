369aa0caf91f07dd5bf48a8f3a55542f613c08b2
NaZe0320 <rwt1226@naver.com>
2026-02-27 14:36:40 +0900

    [feat] 라이트 모드에서 상태바 텍스트 색상 안보이는 문제 해결

--- app/src/main/java/com/naze/do_swipe/ui/theme/Theme.kt
+++ app/src/main/java/com/naze/do_swipe/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.naze.do_swipe.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,8 +9,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.naze.do_swipe.data.preferences.ThemeMode
 
 private val DarkColorScheme = darkColorScheme(
@@ -62,6 +67,18 @@ fun DoSwipeTheme(
         }
         resolvedDarkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        val window = (view.context as Activity).window
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val windowInsetsController: WindowInsetsControllerCompat? =
+            WindowCompat.getInsetsController(window, window.decorView)
+
+        SideEffect {
+            windowInsetsController?.isAppearanceLightStatusBars = !resolvedDarkTheme
+        }
     }
 
     MaterialTheme(
