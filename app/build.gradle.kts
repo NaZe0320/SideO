a10a7fccf5642e4ab1870c1bf2c84d79bd810b44
NaZe0320 <rwt1226@naver.com>
2026-03-06 13:16:07 +0900

    [version] 버전업 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.naze.do_swipe"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.2"
+        versionCode = 6
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
